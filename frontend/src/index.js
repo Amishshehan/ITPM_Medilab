2118a54cf78665e0d68bad38d306de98aba2a3b0
Amishshehan <59696770+Amishshehan@users.noreply.github.com>
2022-05-08 17:31:14 +0530

    sys file update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
-import './index.css';
 import App from './App';
+
+
 
 import { Provider } from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk  from 'redux-thunk';
+import reducers from './reducers';
 
 
-const store = createStore(compose(applyMiddleware(thunk)) )
+const store = createStore(reducers, compose(applyMiddleware(thunk)) )
 
 ReactDOM.render(
   <Provider store = {store}>
