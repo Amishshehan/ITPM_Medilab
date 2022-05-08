19da243a78fc36bc2aa5c6191753f84933499d5e
Amishshehan <59696770+Amishshehan@users.noreply.github.com>
2022-05-08 17:32:20 +0530

    Doctor view commit

---
+++ frontend/src/components/DoctorView.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid, Paper, TextField, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { getDoctors, getDoctorsBySearch } from '../actions/doctors';
+import Pagination from './Pagination';
+import Doctors from './DoctorView/Doctors';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import DocImg from '../images/Doc.jpg'
+import memories from '../images/memories.png';
+import useStyles from '../styles';
+import doctorUseStyles from './stComStyles';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+
+const DoctorCom = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const stClasses = doctorUseStyles();
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        dispatch(getDoctors());
+    }, [currentId, dispatch]);
+
+    const searchDoctor = () => {
+        if (search.trim()) {
+            dispatch(getDoctorsBySearch({ search }));
+            history.push(`/doctorsdetails/search?searchQuery=${search || 'none'}`);
+        } else {
+            history.push('/doctorsdetails')
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {
+            searchDoctor();
+        }
+    };
+
+    return (
+        <Container maxidth="lg">
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={8}>
+                    <div  position="static" color="inherit" style={{ marginTop: '170px', marginBottom: '20px', float:'left',marginLeft:"25px" }}>
+                        <Typography className={classes.heading} varient="h2" align="center">Doctors Information</Typography>
+                    </div>
+                </Grid>
+                <Grid item xs={6} md={4} style={{ marginTop: '140px', marginBottom: '50px', }}>
+                    <AppBar className={stClasses.appBarSearch} position="static" color="inherit" >
+                        <Grid container spacing={2}>
+                            <Grid item xs={6} md={8}>
+                                <TextField
+                                    name="search"
+                                    variant="outlined"
+                                    label="Search Doctors"
+                                    size='small'                                    
+                                    onKeyPress={handleKeyPress}
+                                    fullWidth
+                                    value={search}
+                                    onChange={(e) => setSearch(e.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={6} md={4}>
+                                <Button style={{ marginTop: '0',background:'#2bc4ba'}} onClick={searchDoctor} className={stClasses.searchButton} variant="contained" color="primary">Search</Button>
+                            </Grid>
+                        </Grid>
+                    </AppBar>
+                </Grid>
+            </Grid>
+
+            <Grow in>
+                <Container maxWidth="xl">
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3} className={stClasses.gridContainer}>
+                        <Grid item xs={12} sm={12}>
+                            <div style={{ marginBottom: '50px' }}>
+                                <Card>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            width=""
+                                            height="450"
+                                            image={DocImg}
+                                            alt="green iguana"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                Doctor Information
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                <h6>Our primary goal as health care providers is to place our patients first through clinical excellence and compassionate care. That starts with helping you locate the right consultant to lead you on your journey to better health and well-being. Designed with Modular channel areas for added privacy the Specialist Center provides the widest array of services and a team of expert consultants belonging to numerous medical specialities, providing comprehensive, personalised care.</h6>
+                                                <div style={{ marginLeft: '20px' }}>
+                                                <tr>&#8226; We have specialists in ALL the fields of healthcare at the Medilab.</tr>
+                                                <tr>&#8226; Appointments can be taken over the phone at any time.</tr>
+                                                <tr>&#8226; Hospital enquiry hotline works round the clock.</tr>
+                                                <tr>&#8226; Location guide map is available and directional signboards are fixed on each floor.</tr>
+                                                <tr>&#8226; Channelling and pharmacy facilities are available in each channelling floors.</tr>
+                                                <tr>&#8226; Public Relation Officer helps the patient at the channelling reception or OPD entrance.</tr>
+                                                <tr>&#8226; Facilities for all diagnostic services under the same roof</tr>
+                                                <tr>&#8226; Comfortable waiting space</tr>
+                                                <tr>&#8226; Duty doctors are available on-call round the clock in all specialities</tr>
+                                                </div>
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+
+                            </div>
+
+                            <Doctors setCurrentId={setCurrentId} />
+                        </Grid>
+
+
+                        <Grid item xs={12} sm={12}>
+
+                            <Paper elevation={6} style={{ marginBottom: '40px', marginTop: '50px', marginLeft: '150px', marginRight: '150px' }}>
+                                <Pagination />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+
+        </Container>
+    );
+
+}
+
+export default DoctorCom;
---
+++ frontend/src/components/DoctorView/Doctor/Doctor.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { Card, CardContent, CardMedia, Hidden, Typography } from '@material-ui/core';
+import Button from '@mui/material/Button';
+import "./Styles.css"
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { display } from '@mui/material/node_modules/@mui/system';
+
+const Doctor = ({ doctor, setCurrentId }) => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const pdfGenerate = ({ }) => {
+        var doc = new jsPDF('portrait', 'px', 'a4', 'false');
+
+
+        doc.setFontSize(28);
+        doc.setFont(undefined, 'bold');
+        doc.setTextColor(43, 196, 186)
+        doc.text(152, 60, "Doctor Profile");
+
+        doc.addImage(doctor.selectedFile, 'PNG', 135, 80, 180, 200);
+
+        autoTable(doc, { html: '#my-table' })
+        doc.autoTable({
+            startY: 290,
+            margin: 50,
+            theme: 'plain',
+            columnStyles: { 0: { halign: 'center' } },
+            alternateRowStyles: { fontSize: 18, fontStyle: 'bold' },
+            body: [
+
+                [[doctor.fullName]],
+            ],
+        })
+
+        var date = new Date().toLocaleDateString();
+        var time = new Date().toLocaleTimeString();
+
+        doc.autoTable({
+            startY: 320,
+            margin: 50,
+            headStyles: { fillColor: [43, 196, 186] },
+            columnStyles: { 1: { halign: 'right', cellWidth: 200 } },
+            alternateRowStyles: { fillColor: [220, 245, 243] },
+            head: [['', 'Doctor Details']],
+            body: [
+                ['Full Name', [doctor.fullName]],
+                ['Email', [doctor.email]],
+                ['Phone Number', [doctor.phoneNumber]],
+                ['Gender', [doctor.gender]],
+                ['Date of Birth', [doctor.birthday]],
+                ['SLMC Number', [doctor.slmcNo]],
+                ['Doctor Speciality', [doctor.speciality]],
+                ['Qualifications', [doctor.qualification]],
+
+            ],
+        })
+        doc.autoTable({
+            startY: 560,
+            margin: 50,
+            theme: 'plain',
+
+            columnStyles: { 1: { halign: 'right' } },
+            alternateRowStyles: { fontSize: 8, fontStyle: 'italic' },
+            body: [
+
+                ['Medilab', 'Generated on' + ' ' + [date] + ' ' + [time]],
+            ],
+        })
+        doc.save('Report.pdf');
+    }
+
+
+
+    return (
+        <Card className={classes.card} raised elevation={6}>
+
+            <div class="pt-3 ">
+                <div class="">
+                    <div class=" div1 row py-0 px-0">
+                        <div class="col-3 mt-1">
+                        </div>
+
+                        <div class="col-6 d-flex justify-content-center">
+                            <img className='pofImg' src={doctor.selectedFile} height="" style={{ width: "250px", height: "180px", borderRadius: '3%' }} />
+                        </div>
+                        <div>
+                            <div class=" col-3 d-flex flex-column pl-4 ml-2">
+                                <button class="btn discBtn text-white p-1 p-md-2"
+                                    onClick={pdfGenerate}
+                                ><i class="fas fa-download"></i></button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class=" p-3 ">
+                        <div className='details'>
+                            <h3 style={{ color: "#2bc4ba", textAlign:'center', marginBottom:'15px' }}>{doctor.fullName}</h3>
+                            <info>Speciality:  </info><p className='inf'>{doctor.speciality} </p>
+                            <info>SLMC Number:  </info><p className='inf'>{doctor.slmcNo} </p>
+                            <info>Phone Number: </info><p className='inf'>{doctor.phoneNumber}</p>
+
+
+                        </div>
+
+
+                        <div class="collapse" id="collapseExample">
+                            <info>Email:</info> <p className='inf'>{doctor.email} </p>
+                            <info>Date of Birth: </info><p className='inf'>{doctor.birthday} </p>
+                            <info>Gender: </info><p className='inf'> {doctor.gender} </p>
+                            <info>Qualifications: </info><p className='inf'>{doctor.qualification} </p>
+                        </div>
+                        
+                        <Button
+                            id="read-btn"
+                            style={{ marginTop: '15px', marginLeft: '110px', backgroundColor: "#2bc4ba" }}
+                            variant="contained"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#collapseExample"
+                            aria-expanded="false"
+                            aria-controls="collapseExample">
+                            Read More...
+                        </Button>
+
+                    </div>
+                </div>
+            </div>
+
+        </Card >
+    );
+}
+
+export default Doctor;
